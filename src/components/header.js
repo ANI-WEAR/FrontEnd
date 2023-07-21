6ca7ea15430f12794a341df11316ac29f114b78d
kimkimjunjun <wnsgnl323@gmail.com>
2023-07-21 20:17:37 +0900

    수정

--- src/components/header.js
+++ src/components/header.js
@@ -23,9 +23,9 @@ export default function Header() {
                         {categories.map((category, index) => (
                             <div>
                                 <div className='flex p-3 mx-3' key={index}>
-                                    <span className='text-[1.5rem] font-bold'>{category.ko}</span>
-                                    <span className='ml-3 text-[#949494] text-[1.25rem] font-medium'>{category.en}</span>
-                                    <span className='ml-auto'>+</span>
+                                    <span className='text-[1.5rem] font-bold self-center'>{category.ko}</span>
+                                    <span className='ml-3 text-[#949494] text-[1.25rem] font-medium self-center'>{category.en}</span>
+                                    <span className='ml-auto text-[2rem] font-semibold self-center'>+</span>
                                 </div>
                                 <div className='w-full h-1 border-b border-[#949494]' />
                             </div>
@@ -46,7 +46,7 @@ export default function Header() {
                     <rect x="22" y="25.9565" width="25.0435" height="25.0435" stroke="black" stroke-width="3" />
                     <path d="M40.7826 30.1304V25.2609C40.7826 21.8031 37.9795 19 34.5218 19V19C31.064 19 28.2609 21.8031 28.2609 25.2609V30.1304" stroke="black" stroke-width="3" />
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" width="80" height="70" viewBox="0 0 80 70" fill="none">
+                <svg className='mr-3' xmlns="http://www.w3.org/2000/svg" width="80" height="70" viewBox="0 0 80 70" fill="none">
                     <path d="M53.4348 51V38.4782H27V51" stroke="black" stroke-width="3" />
                     <ellipse cx="39.5218" cy="25.9565" rx="6.95652" ry="6.95652" stroke="black" stroke-width="3" />
                 </svg>
